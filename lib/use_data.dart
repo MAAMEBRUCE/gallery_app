a44f471cdad94591edfc3fd2723102aea91f8278
MAAMEBRUCE <esibrucejane@gmail.com>
2021-09-17 16:20:40 -0800

    Final gallery app project

---
+++ lib/use_data.dart
@@ -0,0 +1,23 @@
+List userData = [
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/profile_1.jpg",
+    "userPost": "images/picture_1.jpg",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/profile_2.jpg",
+    "userPost": "images/picture_2.jpg",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/profile_3.jpg",
+    "userPost": "images/picture_3.jpg",
+    "userPostLikes": "23",
+  }
+];
